fc92c03196b43a53e06019fcdd4b883184a2bafc
Marchi135 <marvelchir@gmail.com>
2023-08-16 07:51:29 +0200

    add function

--- scripts.js
+++ scripts.js
@@ -1,52 +1,53 @@
 const data = {
-lists: [
-['first', [15, 11, 13, 7, 5]],
-['second', [2, 6, 8, 4, 14, 12, 10]],
-['third', [9, 3, 1]],
-]
-};
-  
+	lists: [
+		['first', [15, 11, 13, 7, 5]],
+		['second', [2, 6, 8, 4, 14, 12, 10]],
+		['third', [9, 3, 1]],
+	]
+}
+
 // Only edit below
-const { first = 1 } = data.first || {}
-const { second = 1 } = data.second || {}
-const { third = 1 } = data.third || {}
-  
-  const result = [];
-  
-  const extractBiggest = () => {
-    const firstLast = first.length ? first[first.length - 1] : -Infinity;
-    const secondLast = second.length ? second[second.length - 1] : -Infinity;
-    const thirdLast = third.length ? third[third.length - 1] : -Infinity;
-  
-    if (firstLast >= secondLast && firstLast >= thirdLast) {
-      return first.pop();
-    }
-  
-    if (secondLast >= firstLast && secondLast >= thirdLast) {
-      return second.pop();
-    }
-  
-    return third.pop();
-  };
-  
-  // Only edit above
-  
-  result.push(extractBiggest());
-  result.push(extractBiggest());
-  result.push(extractBiggest());
-  result.push(extractBiggest());
-  result.push(extractBiggest());
-  
-  result.push(extractBiggest());
-  result.push(extractBiggest());
-  result.push(extractBiggest());
-  result.push(extractBiggest());
-  result.push(extractBiggest());
-  
-  result.push(extractBiggest());
-  result.push(extractBiggest());
-  result.push(extractBiggest());
-  result.push(extractBiggest());
-  result.push(extractBiggest());
-  
-  console.log(result);
+const result = [];
+const extractBiggest = () => {
+	let max = -Infinity;
+	let maxIndex = -1;
+
+	data.lists.forEach(([_, list], index) => {
+		if (list.length > 0 && list[list.length - 1] > max) {
+			max = list[list.length - 1];
+			maxIndex = index;
+		}
+	});
+
+	if (maxIndex !== -1) {
+		const [_, list] = data.lists[maxIndex];
+		return list.pop();
+	} else {
+		return 1; // Default value if all lists are empty
+	}
+};
+
+// Only edit above
+
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+
+/*for (let i = 0; i < 15; i++) {
+	result.push(extractBiggest());
+}*/
+console.log(result)
